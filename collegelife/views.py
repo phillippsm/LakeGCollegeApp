4dcec138c926ecfa381011d25a8e79b239110a66
phillippsm <matthew.phillipps@ed.act.edu.au>
2018-05-17 07:22:42 +1000

    First Views

--- collegelife/views.py
+++ collegelife/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Clubs
 
 def home(request):
-  return HttpResponse('This is College Life!')
+  clubs = Clubs.objects.all()
+  return render(request, 'home.html', {'clubs': clubs})
 
 # Create your views here.
